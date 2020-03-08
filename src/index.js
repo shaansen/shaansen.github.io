1e783439940b9ebead27cdec1858302e5727b100
shaansen <shantanu.sengupta2008@gmail.com>
2020-03-07 21:52:53 -0800

    Added slider + fonts + styling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
+import './slider.scss';
 
 const App = () => {
 	const [darkTheme, setDarkTheme] = useState(false);
 	return (
 		<div className={darkTheme ? 'darkContainer' : 'lightContainer'}>
 			<div className='header'>
-				<button onClick={() => setDarkTheme(!darkTheme)}>Light Theme</button>
+				<i className='fa fa-moon-o'></i>
+				<label className='switch'>
+					<input type='checkbox' onChange={() => setDarkTheme(!darkTheme)} />
+					<span className='slider round'></span>
+				</label>
+				<i className='fa fa-sun-o'></i>
 			</div>
 			<div className='main-content'>
-				<img className='profile-picture' src='src/profile.jpg' alt='Girl in a jacket' />
-				<h1>Shantanu Sengupta</h1>
-				<p>Front End Engineer @ AWS</p>
+				<div className='profile-picture'>
+					<img src='src/profile.jpg' alt='Girl in a jacket' />
+				</div>
+				<div className='text-container'>
+					<h1>Shantanu Sengupta</h1>
+					<p>
+						Front End Engineer @ AWS
+						<br /> Competitive Coding Enthusiast
+						<br /> Data visualization extraordinaire
+						<br /> Amateur chef
+						<br /> Voracious reader
+						<br /> Video game noob
+						<br /> Photography dabbler
+					</p>
+				</div>
 			</div>
 		</div>
 	);
